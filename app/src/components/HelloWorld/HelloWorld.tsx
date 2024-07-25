5e1e8c5878838faf2b08edf6928dee79773ec2d8
ng <n.guerns@gmail.com>
2024-07-24 21:27:53 -0500

    chore: removed hello world component

--- app/src/components/HelloWorld/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./HelloWorld.module.scss";
-
-export default function HelloWorld() {
-  return (
-    <div className={styles.helloWorld}>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
