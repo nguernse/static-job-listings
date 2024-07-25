a0253f5778042fee3b03af7ede34f992e011504f
ng <n.guerns@gmail.com>
2024-07-24 21:27:32 -0500

    chore(ui): updated card component

--- app/src/components/Card/Card.tsx
+++ app/src/components/Card/Card.tsx
@@ -1,17 +1,24 @@
-import { PropsWithChildren } from "react";
+import { HTMLAttributes } from "react";
 
 import styles from "./Card.module.scss";
 import clsx from "clsx";
 
-type Props = PropsWithChildren<{
+type Props = HTMLAttributes<HTMLDivElement> & {
   active?: boolean;
-}>;
+  className?: string;
+};
 
-export default function Card({ active = false, children }: Props) {
+export default function Card({
+  active = false,
+  children,
+  className,
+  ...props
+}: Props) {
   return (
     <div
-      className={clsx(styles.Card, { [styles.active]: active })}
+      className={clsx(styles.Card, { [styles.active]: active }, className)}
       data-active={active}
+      {...props}
     >
       {children}
     </div>
